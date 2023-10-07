6d9384f4f802d10b647883e606e51dc3565454e4
Glaucus Almeida <glaucusal@gmail.com>
2023-10-07 12:14:12 -0300

    Service commit

---
+++ services/ServicoProduto.ts
@@ -0,0 +1,47 @@
+import { Product } from '../models/ProductModel'
+import { ProductInterface } from '../types/InterfaceProduto'
+
+import * as path_node from 'path'
+
+const path = path_node.resolve('./data');
+const filename = 'estoque.csv';
+
+export class ProductService{
+
+    static async getProducts() : Promise<ProductInterface[]> {
+        // Busca atraves do service
+        return await Product.read(path, filename);
+    }
+
+    static async saveProduct(prod : ProductInterface){
+        
+        // Valida as entradas do Produto
+        if(prod.nome === '' || prod.nome == undefined ||
+        prod.peso == undefined ||
+        prod.valor == undefined || 
+        prod.quantidade == undefined){
+            throw new Error('Todos os campos devem ser preenchidos!');
+        }
+
+        // Busca os produtos salvos e verifica se já existe algum que possui o mesmo identificador
+        let res = await Product.findOne(path, filename, prod.nome);
+        
+        if(res) throw new Error('Produto já existe no inventário!');
+         
+        // Salva o produto
+        await Product.create(path, filename, prod);
+    }
+
+    static async deleteProduct(name : string){
+        
+        // const found = Product.findOne(path, name);
+        // if(!found) throw new Error('Produto não encontrado!');
+
+        await Product.delete(path, filename, name);
+    }
+
+    static async find(name : string) : Promise<ProductInterface | undefined>{
+        return await Product.findOne(path, filename, name);
+    }
+
+}
